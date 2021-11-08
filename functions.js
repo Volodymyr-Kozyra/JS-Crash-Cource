436f181e83e53dc6e7452526b5c5a1a533b13c79
Volodymyr Kozyra <ibis82@gmail.com>
2021-11-08 18:17:35 +0200

    commit 3

--- functions.js
+++ functions.js
@@ -1,12 +1,20 @@
 //hp and vitality
 let hp = 170;
+const fighter1 = {
+    name: 'Tank',
+    apHeight: 185,
+    apWeight: 120,
+    agility: 65,
+    hp: 110,
+    damage: 9,
+    isHeDanger: false,
+    hit: () => console.log ("miss a hit"),
+}
 function vitality(){
-totalvit = hp * 3;
+totalvit = fighter1.hp * 3;
 console.log("total helth of the figther is: ", totalvit);
 
 }
-
-vitality();
 
 // arrow fn
 let hp2 = 100;
@@ -20,3 +28,9 @@ const totHp2 = function(){
 
 }
 console.log("total helth of the figther is: ", totHp2());
+
+
+///test on the fighter
+
+
+vitality();
