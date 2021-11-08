e92876752b4f03d220ddf61cfcccaa8a58b1589a
Volodymyr Kozyra <ibis82@gmail.com>
2021-11-08 17:46:14 +0200

    commit two

--- functions.js
+++ functions.js
@@ -1,1 +1,22 @@
-//some functions
+//hp and vitality
+let hp = 170;
+function vitality(){
+totalvit = hp * 3;
+console.log("total helth of the figther is: ", totalvit);
+
+}
+
+vitality();
+
+// arrow fn
+let hp2 = 100;
+const totHp = () => hp2 * 3;
+console.log("total helth of the figther is: ", totHp());
+
+// expression
+let hp3 = 100
+const totHp2 = function(){
+    return hp3 * 3;
+
+}
+console.log("total helth of the figther is: ", totHp2());
